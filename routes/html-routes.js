e9ebfabc8fadf68ceb2b4ca76e96f1578655ab76
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-06-19 15:31:10 -0400

    Add statistics route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 
-
+// Statistics
+router.get('/stats', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
