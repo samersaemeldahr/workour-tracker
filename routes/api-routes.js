5ec5cac65c09a188bfed6e5cc950306e1cf670d1
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-06-19 12:42:35 -0400

    Find all workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
+
+// Find all
+router.get('/api/workouts', (req, res) => {
+	Workout.find()
+		.then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
