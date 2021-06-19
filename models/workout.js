3bc17acc24e72afe232f21f4072463e3474796d9
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-06-18 22:45:02 -0400

    Create a new workout schema

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        name: {
+            type: String,
+            required: 'Please enter an exercise name'
+        },
+        type: {
+            type: String,
+            required: 'Please enter an exercise type'
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
