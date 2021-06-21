05fc961770d4c1772dbb18a6df3dc713b300f086
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-06-20 23:59:49 -0400

    Fix connection

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_db', { useNewUrlParser: true });
-
 app.use(require('./routes/api-routes.js'));
 app.use(require('./routes/html-routes.js'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
